f914aae26cedc6bd3d4a030f75369e1d50ec7a3b
KantaSwift <github@kanta-_-soccer.docomo.ne.jp>
2022-09-28 16:41:48 +0900

    コードのみで実装できるように設定

---
+++ SwiftPractice/tinderClone/tinderClone/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  tinderClone
+//
+//  Created by 上條栞汰 on 2022/09/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+}
+
