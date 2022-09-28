540ac59c739c09d4e400a9dfd72ad026b8fb2ec0
KantaSwift <github@kanta-_-soccer.docomo.ne.jp>
2022-09-28 17:02:46 +0900

    画面の構成を決めた

--- SwiftPractice/tinderClone/tinderClone/ViewController.swift
+++ SwiftPractice/tinderClone/tinderClone/ViewController.swift
@@ -8,11 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let view1: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        return view
+    }()
+    
+    private let view2: UIView = {
+        let view = UIView()
+        view.backgroundColor = .blue
+        return view
+    }()
+    
+    private let view3: UIView = {
+        let view = UIView()
+        view.backgroundColor = .yellow
+        return view
+    }()
+    
+    private lazy var layoutStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [view1, view2, view3])
+        stackView.axis = .vertical
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBackground
+        setupViews()
+        setupConstraint()
     }
-
+    
+    private func setupViews() {
+        view.addSubview(layoutStackView)
+    }
+    
+    private func setupConstraint() {
+        [
+            view1.heightAnchor.constraint(equalToConstant: 100),
+            view3.heightAnchor.constraint(equalToConstant: 120),
+            
+            layoutStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            layoutStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            layoutStackView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            layoutStackView.rightAnchor.constraint(equalTo: view.rightAnchor)
+        ].forEach { $0.isActive = true }
+    }
 }
 
